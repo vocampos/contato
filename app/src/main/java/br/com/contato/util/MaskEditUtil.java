2ce6f2d52a6f2a692dcce65739bb801b2fc7ff21
Valbert Campos <valbert.sistemas@gmail.com>
2018-03-07 02:14:45 -0300

    exemplos implementados para aula 05/03

--- app/src/main/java/br/com/contato/util/MaskEditUtil.java
+++ app/src/main/java/br/com/contato/util/MaskEditUtil.java
@@ -4,6 +4,9 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public abstract class MaskEditUtil {
 
     public static final String FORMAT_CPF = "###.###.###-##";
@@ -62,4 +65,59 @@ public abstract class MaskEditUtil {
     public static String unmask(final String s) {
         return s.replaceAll("[.]", "").replaceAll("[-]", "").replaceAll("[/]", "").replaceAll("[(]", "").replaceAll("[ ]","").replaceAll("[)]", "");
     }
+
+
+    public static TextWatcher monetario(final EditText campo) {
+        return new TextWatcher() {
+
+            private boolean isUpdating = false;
+            // Pega a formatacao do sistema, se for brasil R$ se EUA US$
+            private NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int after) {
+                // Evita que o método seja executado varias vezes.
+                // Se tirar ele entre em loop
+                if (isUpdating) {
+                    isUpdating = false;
+                    return;
+                }
+
+                isUpdating = true;
+                String str = s.toString();
+                // Verifica se já existe a máscara no texto.
+                boolean hasMask = ((str.indexOf("R$") > -1 || str.indexOf("$") > -1) &&
+                        (str.indexOf(".") > -1 || str.indexOf(",") > -1));
+                // Verificamos se existe máscara
+                if (hasMask) {
+                    // Retiramos a máscara.
+                    str = str.replaceAll("[R$]", "").replaceAll("[,]", "")
+                            .replaceAll("[.]", "");
+                }
+
+                try {
+                    // Transformamos o número que está escrito no EditText em
+                    // monetário.
+                    str = nf.format(Double.parseDouble(str) / 100);
+
+                    campo.setText((str.substring(0, str.indexOf("$") + 1)) + " " + (str.replace("R$", "")));
+                    campo.setSelection(campo.getText().length());
+                } catch (NumberFormatException e) {
+                    s = "";
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+    }
+
 }
--- app/src/main/java/br/com/contato/util/Util.java
+++ app/src/main/java/br/com/contato/util/Util.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Base64;
@@ -21,6 +22,10 @@ import java.util.List;
 import br.com.contato.modelo.CameraResultado;
 
 public final class Util {
+
+    public enum CameraOptions {
+        CAMERA_ONLY, GALLERY_ONLY, GALLERY_CAMERA, GET_IMAGE_CODE;
+    }
 
     public static void preencherBase64(String base64, ImageView imageView) {
         byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
@@ -82,10 +87,8 @@ public final class Util {
 
             if (myBitmap != null) {
 
-                myBitmap = Bitmap.createScaledBitmap(myBitmap, 500, 500, false);
-
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                myBitmap.compress(Bitmap.CompressFormat.JPEG, 100,new ByteArrayOutputStream());
+                myBitmap.compress(Bitmap.CompressFormat.JPEG, 100,baos);
 
                 result = new CameraResultado();
                 result.setFotoBase64(Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP));
@@ -121,5 +124,28 @@ public final class Util {
         ActivityCompat.requestPermissions(activity, newPermissions, requestCode);
 
         return false;
+    }
+
+    public static void openPhoto(CameraOptions options, Activity activity) {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        galleryIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Intent chooserIntent = Intent.createChooser(galleryIntent, "Continuar usando:");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{cameraIntent});
+
+        if (options == CameraOptions.CAMERA_ONLY) {
+            activity.startActivityForResult(cameraIntent, CameraOptions.GET_IMAGE_CODE.ordinal());
+            return;
+        }
+        if (options == CameraOptions.GALLERY_ONLY) {
+            activity.startActivityForResult(galleryIntent, CameraOptions.GET_IMAGE_CODE.ordinal());
+            return;
+        }
+        activity.startActivityForResult(chooserIntent, CameraOptions.GET_IMAGE_CODE.ordinal());
+        return;
     }
 }
